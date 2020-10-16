af701f3d7a792f7b8d9c0038c560b7592bbceaa9
Elvin Kosova <elvinkosova@gmail.com>
2020-10-16 06:56:35 -0700

    Improve code

--- test/sort/shuffle.test.js
+++ test/sort/shuffle.test.js
@@ -4,7 +4,7 @@ describe('shuffle', () => {
   test('should randomize a sorted array', () => {
     const sorted = [1, 2, 3, 4, 5];
     sort.shuffle.shuffle(sorted);
-    console.log(sorted);
+    // console.log(sorted);
     expect(sort.util.isSorted(sorted)).toBeFalsy();
   });
 });
