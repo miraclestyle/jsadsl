a0871bfc39d5011240f75a735d5b934a8041b271
Elvin Kosova <elvinkosova@gmail.com>
2020-10-17 03:54:56 -0700

    Implement insertion sort

---
+++ test/sort/insertion.test.js
@@ -0,0 +1,32 @@
+const sort = require('../../lib/sort');
+
+describe('insertion', () => {
+  test('should sort a single element array', () => {
+    const array = [1];
+    sort.insertion(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should not unsort a sorted array', () => {
+    const array = [1, 2, 3, 4, 5];
+    sort.insertion(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should sort an array', () => {
+    const array = [5, 3, 1, 4, 2];
+    sort.insertion(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should sort an array in descending order', () => {
+    const array = [5, 3, 1, 4, 2];
+    const compare = (a, b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    };
+    sort.insertion(array, compare);
+    expect(sort.isSorted(array, compare)).toBeTruthy();
+  });
+});
