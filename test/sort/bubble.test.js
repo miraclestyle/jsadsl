8ab152d9db7a431c8db87bf3ab8caeefaa6dd634
Elvin Kosova <elvinkosova@gmail.com>
2020-10-17 04:14:34 -0700

    Implement bubble sort

---
+++ test/sort/bubble.test.js
@@ -0,0 +1,32 @@
+const sort = require('../../lib/sort');
+
+describe('bubble', () => {
+  test('should sort a single element array', () => {
+    const array = [1];
+    sort.bubble(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should not unsort a sorted array', () => {
+    const array = [1, 2, 3, 4, 5];
+    sort.bubble(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should sort an array', () => {
+    const array = [5, 3, 1, 4, 2];
+    sort.bubble(array);
+    expect(sort.isSorted(array)).toBeTruthy();
+  });
+
+  test('should sort an array in descending order', () => {
+    const array = [5, 3, 1, 4, 2];
+    const compare = (a, b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    };
+    sort.bubble(array, compare);
+    expect(sort.isSorted(array, compare)).toBeTruthy();
+  });
+});
