cf5db98c6b820a2323199b5330698bfb753da03a
Elvin Kosova <elvinkosova@gmail.com>
2020-10-26 16:23:50 -0700

    Implement key index counting

---
+++ test/sort/radix-sort.test.js
@@ -0,0 +1,59 @@
+const sort = require('../../lib/sort');
+
+const sorts = ['keyIndexCounting'];
+let array = [];
+const n = 10;
+
+const init = (low = 0, high = n) => {
+  const output = [];
+  for (let i = low; i < high; i += 1) {
+    const r = sort.util.randomInt(97, 123);
+    const char = String.fromCharCode(r);
+    output.push(char);
+  }
+  return output;
+};
+
+describe.each(sorts)('%s sort', (name) => {
+  beforeEach(() => {
+    array = init();
+  });
+
+  test('should sort a single element array', () => {
+    array = init(1, 2);
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+
+  test('should not unsort a sorted array', () => {
+    array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+
+  test('should sort an array', () => {
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+
+  test('should sort an array in descending order', () => {
+    const compare = (a, b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    };
+    const transform = (value, index) => {
+      const reference = ('z').charCodeAt(0);
+      const code = value.charCodeAt(index);
+      return reference - code;
+    };
+    sort[name](array, transform);
+    expect(sort.isSorted(array, compare)).toBe(true);
+  });
+
+  test('should sort a randomized array of 20,000 items', () => {
+    array = init(0, 20000);
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+});
