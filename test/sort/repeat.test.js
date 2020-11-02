8498b0e8d548aabf49dc3c1905b6b8f0682902a2
Elvin Kosova <elvinkosova@gmail.com>
2020-11-02 13:38:35 -0800

    Repeat insertion sort

--- test/sort/repeat.test.js
+++ test/sort/repeat.test.js
@@ -1,46 +1,73 @@
 const sort = require('../../lib/sort');
 
+const sorts = ['repeat'];
 let array = [];
 const n = 10;
 
 const init = (low = 0, high = n) => {
   const output = [];
   for (let i = low; i < high; i += 1) {
-    output.push(i);
+    output.push(sort.util.randomInt(i, n));
   }
   return output;
 };
 
-describe('shuffle', () => {
+describe.each(sorts)('%s sort', (name) => {
   beforeEach(() => {
     array = init();
   });
 
-  test('should randomize an empty array', () => {
-    array = init(0, 0);
-    sort.repeat(array);
-    expect(sort.isSorted(array)).toBe(true);
-  });
-
-  test('should randomize a sorted array with single element', () => {
+  test('should sort a single element array', () => {
     array = init(1, 2);
-    sort.repeat(array);
+    sort[name](array);
     expect(sort.isSorted(array)).toBe(true);
   });
 
-  test('should randomize a sorted array', () => {
-    sort.repeat(array);
-    expect(sort.isSorted(array)).toBe(false);
+  test('should not unsort a sorted array', () => {
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
   });
 
-  test('should partialy randomize a sorted array with custom range', () => {
-    sort.repeat(array, 3, 8);
-    expect(sort.isSorted(array, undefined, 3, 8)).toBe(false);
+  test('should sort an array', () => {
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
   });
 
-  test('should randomize a sorted array of 10,000 items', () => {
+  test('should partialy sort an array with custom range', () => {
+    sort[name](array, undefined, 3, 8);
+    expect(sort.isSorted(array, undefined, 3, 8)).toBe(true);
+  });
+
+  test('should sort an array in descending order', () => {
+    const compare = (a, b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    };
+    sort[name](array, compare);
+    expect(sort.isSorted(array, compare)).toBe(true);
+  });
+
+  test('should sort a randomized array of 10,000 items', () => {
     array = init(0, 10000);
-    sort.repeat(array);
-    expect(sort.isSorted(array)).toBe(false);
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+
+  test('should sort a randomized array of 1,000,000 items with fast sorts', () => {
+    const fastSorts = new Set([
+      'heap',
+      'merge',
+      'quick',
+      'threeWayQuick',
+    ]);
+    if (fastSorts.has(name)) {
+      array = init(0, 1000000);
+      sort[name](array);
+      expect(sort.isSorted(array)).toBe(true);
+    } else {
+      expect(true).toBe(true);
+    }
   });
 });
