1483c84aedbffd27808134da84e8c09daf400e88
Elvin Kosova <elvinkosova@gmail.com>
2020-11-03 14:03:47 -0800

    Implement least significant digit first radix sort

--- test/sort/radix-sort.test.js
+++ test/sort/radix-sort.test.js
@@ -1,26 +1,38 @@
 const sort = require('../../lib/sort');
 
-const sorts = ['keyIndexCounting'];
+const sorts = ['keyIndexCounting', 'lsd'];
 let array = [];
 const n = 10;
+const s = 5;
 
-const init = (low = 0, high = n) => {
+const init = (low = 0, high = n, w = s) => {
   const output = [];
   for (let i = low; i < high; i += 1) {
-    const r = sort.util.randomInt(97, 123);
-    const char = String.fromCharCode(r);
-    output.push(char);
+    let str = '';
+    for (let j = 0; j < w; j += 1) {
+      const r = sort.util.randomInt(97, 123);
+      str += String.fromCharCode(r);
+    }
+    output.push(str);
   }
   return output;
 };
 
 describe.each(sorts)('%s sort', (name) => {
   beforeEach(() => {
-    array = init();
+    if (name === 'keyIndexCounting') {
+      array = init(0, n, 1);
+    } else {
+      array = init();
+    }
   });
 
   test('should sort a single element array', () => {
-    array = init(1, 2);
+    if (name === 'keyIndexCounting') {
+      array = init(1, 2, 1);
+    } else {
+      array = init(1, 2);
+    }
     sort[name](array);
     expect(sort.isSorted(array)).toBe(true);
   });
@@ -37,7 +49,7 @@ describe.each(sorts)('%s sort', (name) => {
   });
 
   test('should partialy sort an array with custom range', () => {
-    sort[name](array, undefined, 0, 3, 8);
+    sort[name](array, undefined, 3, 8);
     expect(sort.isSorted(array, undefined, 3, 8)).toBe(true);
   });
 
@@ -57,7 +69,21 @@ describe.each(sorts)('%s sort', (name) => {
   });
 
   test('should sort a randomized array of 10,000 items', () => {
-    array = init(0, 10000);
+    if (name === 'keyIndexCounting') {
+      array = init(0, 10000, 1);
+    } else {
+      array = init(0, 10000);
+    }
+    sort[name](array);
+    expect(sort.isSorted(array)).toBe(true);
+  });
+
+  test('should sort a randomized array of 1,000,000 items', () => {
+    if (name === 'keyIndexCounting') {
+      array = init(0, 1000000, 1);
+    } else {
+      array = init(0, 1000000);
+    }
     sort[name](array);
     expect(sort.isSorted(array)).toBe(true);
   });
