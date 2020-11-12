6bf0d94a87b0c888a72c5f179e690a1985fb7671
Elvin Kosova <elvinkosova@gmail.com>
2020-11-11 17:29:11 -0800

    Repeat is sorted

--- test/sort/repeat.test.js
+++ test/sort/repeat.test.js
@@ -1,70 +1,62 @@
 const sort = require('../../lib/sort');
 
-const sorts = ['repeat'];
 let array = [];
 const n = 10;
 
-const init = (low = 0, high = n) => {
+const init = (low = 0, high = n, reverse = false) => {
   const output = [];
-  for (let i = low; i < high; i += 1) {
-    output.push(sort.util.randomInt(i, n));
+  if (reverse) {
+    for (let i = high - 1; i >= low; i -= 1) {
+      output.push(i);
+    }
+  } else {
+    for (let i = low; i < high; i += 1) {
+      output.push(i);
+    }
   }
   return output;
 };
 
-describe.each(sorts)('%s sort', (name) => {
+describe('isSorted', () => {
   beforeEach(() => {
     array = init();
   });
 
-  test('should sort a single element array', () => {
+  test('shoud return true for an empty array', () => {
+    array = init(0, 0);
+    expect(sort.repeat(array)).toBe(true);
+  });
+
+  test('shoud return true for a single element array', () => {
     array = init(1, 2);
-    sort[name](array);
-    expect(sort.isSorted(array)).toBe(true);
+    expect(sort.repeat(array)).toBe(true);
   });
 
-  test('should not unsort a sorted array', () => {
-    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    sort[name](array);
-    expect(sort.isSorted(array)).toBe(true);
+  test('shoud return true for a sorted array', () => {
+    expect(sort.repeat(array)).toBe(true);
   });
 
-  test('should sort an array', () => {
-    sort[name](array);
-    expect(sort.isSorted(array)).toBe(true);
+  test('shoud return false for an unsorted array', () => {
+    sort.shuffle(array);
+    expect(sort.repeat(array)).toBe(false);
   });
 
-  test('should partialy sort an array with custom range', () => {
-    sort[name](array, undefined, 3, 8);
-    expect(sort.isSorted(array, undefined, 3, 8)).toBe(true);
+  test('should return true for a partially sorted array with custom range', () => {
+    expect(sort.repeat(array, undefined, 3, 8)).toBe(true);
   });
 
-  test('should sort an array in descending order', () => {
+  test('should return false for a reverse sorted array', () => {
+    array = init(0, n, true);
+    expect(sort.repeat(array)).toBe(false);
+  });
+
+  test('should return true for a reverse sorted array with custom comparison function', () => {
+    array = init(0, n, true);
     const compare = (a, b) => {
       if (a > b) return -1;
       if (a < b) return 1;
       return 0;
     };
-    sort[name](array, compare);
-    expect(sort.isSorted(array, compare)).toBe(true);
-  });
-
-  test('should sort a randomized array of 10,000 items', () => {
-    array = init(0, 10000);
-    sort[name](array);
-    expect(sort.isSorted(array)).toBe(true);
-  });
-
-  test('should sort a randomized array of 1,000,000 items with fast sorts', () => {
-    const fastSorts = new Set([
-      'quick',
-    ]);
-    if (fastSorts.has(name)) {
-      array = init(0, 1000000);
-      sort[name](array);
-      expect(sort.isSorted(array)).toBe(true);
-    } else {
-      expect(true).toBe(true);
-    }
+    expect(sort.repeat(array, compare)).toBe(true);
   });
 });
