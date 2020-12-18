2150d26eda8a8438914085afcd8c609d148c2fb7
miraclestyle <elvinkosova@gmail.com>
2020-12-18 20:32:46 +0000

    improve sort testing

--- test/sort/sort.test.js
+++ test/sort/sort.test.js
@@ -3,7 +3,7 @@ const util = require('../../lib/util');
 
 const sorts = ['bubble', 'selection', 'insertion', 'shell', 'heap', 'merge', 'bottomUpMerge', 'quick', 'threeWayQuick'];
 let array = [];
-const n = 10;
+const n = 100;
 
 const init = (low = 0, high = n) => {
   const output = [];
@@ -25,7 +25,18 @@ describe.each(sorts)('%s sort', (name) => {
   });
 
   test('should not unsort a sorted array', () => {
-    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    array = [
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+      10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+      30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+      50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+      60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+      70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+      80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+      90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+    ];
     sort[name](array);
     expect(sort.isSorted(array)).toBe(true);
   });
