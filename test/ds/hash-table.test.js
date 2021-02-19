83205cbd205fdd69a4ab512b04cfabab1b9fa6fd
miraclestyle <elvinkosova@gmail.com>
2021-02-19 11:22:57 +0000

    Implement linear probing table

--- test/ds/hash-table.test.js
+++ test/ds/hash-table.test.js
@@ -1,6 +1,6 @@
 const { ds } = require('../../lib');
 
-const structures = ['separateChainingHashTable'];
+const structures = ['separateChainingHashTable', 'linearProbingHashTable'];
 let ht = null;
 
 describe.each(structures)('%s', (name) => {
@@ -17,21 +17,21 @@ describe.each(structures)('%s', (name) => {
   });
 
   test('should add one item to an empty hash table', () => {
-    ht.set('one hundred', 100);
+    ht.put('one hundred', 100);
     expect(ht.size()).toBe(1);
   });
 
   test('should add two items to an empty hash table', () => {
-    ht.set('two', 2);
-    ht.set('one', 1);
+    ht.put('two', 2);
+    ht.put('one', 1);
     expect(ht.contains('one')).toBe(true);
     expect(ht.contains('two')).toBe(true);
   });
 
   test('should update an added item in a hash table', () => {
-    ht.set('one', 3);
-    ht.set('one', 2);
-    ht.set('one', 1);
+    ht.put('one', 3);
+    ht.put('one', 2);
+    ht.put('one', 1);
     expect(ht.contains('one')).toBe(true);
     expect(ht.get('one')).toBe(1);
     expect(ht.empty()).toBe(false);
@@ -39,18 +39,18 @@ describe.each(structures)('%s', (name) => {
   });
 
   test('should report non-empty for a hash table with one item', () => {
-    ht.set('one hundred', 100);
+    ht.put('one hundred', 100);
     expect(ht.empty()).toBe(false);
   });
 
   test('should report size of 1 for a hash table with one item', () => {
-    ht.set('one hundred', 100);
+    ht.put('one hundred', 100);
     expect(ht.size()).toBe(1);
   });
 
   test('should add multiple items to an empty hash table', () => {
     for (let i = 1; i <= 10; i += 1) {
-      ht.set((i * 10).toString(), i * 10);
+      ht.put((i * 10).toString(), i * 10);
     }
     for (let i = 10; i <= 100; i += 10) {
       expect(ht.contains(i.toString())).toBe(true);
@@ -64,7 +64,7 @@ describe.each(structures)('%s', (name) => {
   });
 
   test('should remove the last item from a non-empty hash table', () => {
-    ht.set('one hundred', 100);
+    ht.put('one hundred', 100);
     ht.remove('one hundred');
     expect(ht.empty()).toBe(true);
   });
@@ -72,7 +72,7 @@ describe.each(structures)('%s', (name) => {
   test('should remove an item from a non-empty hash table', () => {
     const refSet = new Set();
     for (let i = 1; i <= 10; i += 1) {
-      ht.set((i * 10).toString(), i * 10);
+      ht.put((i * 10).toString(), i * 10);
       refSet.add((i * 10).toString());
     }
     for (let i = 10; i <= 100; i += 10) {
@@ -88,7 +88,7 @@ describe.each(structures)('%s', (name) => {
   test('should remove multiple items from a non-empty hash table', () => {
     const refSet = new Set();
     for (let i = 1; i <= 10; i += 1) {
-      ht.set((i * 10).toString(), i * 10);
+      ht.put((i * 10).toString(), i * 10);
       refSet.add((i * 10).toString());
     }
     for (let i = 10; i <= 100; i += 10) {
@@ -111,7 +111,7 @@ describe.each(structures)('%s', (name) => {
 
   test('should remove all items from a non-empty hash table', () => {
     for (let i = 1; i <= 10; i += 1) {
-      ht.set((i * 10).toString(), i * 10);
+      ht.put((i * 10).toString(), i * 10);
     }
     for (let i = 10; i <= 100; i += 10) {
       expect(ht.contains(i.toString())).toBe(true);
@@ -125,7 +125,7 @@ describe.each(structures)('%s', (name) => {
   test('should iterate over multiple items in a non-empty hash table', () => {
     const refSet = new Set();
     for (let i = 1; i <= 10; i += 1) {
-      ht.set((i * 10).toString(), i * 10);
+      ht.put((i * 10).toString(), i * 10);
       refSet.add((i * 10).toString());
     }
     ht.forEach((key) => {
