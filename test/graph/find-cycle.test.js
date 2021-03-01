b4e09d7ed5521354bcd769275727fbd671283747
miraclestyle <elvinkosova@gmail.com>
2021-03-01 15:36:49 +0000

    Consolidate find cycle algorithms

--- test/graph/find-cycle.test.js
+++ test/graph/find-cycle.test.js
@@ -24,4 +24,26 @@ describe.each(names)('%s', (name) => {
       done();
     });
   });
+
+  test('should not detect cycle in a directed acyclic graph', (done) => {
+    Graph('tinyDAG.txt', true).then((g) => {
+      const cycle = graph[name](g);
+      expect(cycle).toBe(null);
+      done();
+    });
+  });
+
+  test('should detect cycle in a directed graph', (done) => {
+    Graph('tinyDigraph.txt', true).then((g) => {
+      const cycle = graph[name](g);
+      expect(cycle).not.toBe(null);
+      let s = '';
+      while (!cycle.empty()) {
+        s += (cycle.pop()).toString();
+        if (!cycle.empty()) s += '->';
+      }
+      expect(s).toBe('3->5->4->3');
+      done();
+    });
+  });
 });
