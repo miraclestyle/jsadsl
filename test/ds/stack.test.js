5395d2568a80dccdbdd61c620c52beb5e5d2923b
miraclestyle <elvinkosova@gmail.com>
2021-04-08 22:21:27 +0000

    Repeat shell sort

--- test/ds/stack.test.js
+++ test/ds/stack.test.js
@@ -2,7 +2,7 @@ const { ds, repeat } = require('../../lib');
 
 ds.repeat = repeat;
 
-const structures = ['LinkedStack', 'ArrayStack', 'repeat'];
+const structures = ['LinkedStack', 'ArrayStack'];
 let stack = null;
 
 describe.each(structures)('%s', (name) => {
